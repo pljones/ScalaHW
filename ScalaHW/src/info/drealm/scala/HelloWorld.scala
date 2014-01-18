5120fce6864290703dc144f7f52e31fd5b35146b
Peter L Jones <peter@drealm.info>
2014-01-18 20:49:26 +0000

    Bit of a rewrite just for fun

--- ScalaHW/src/info/drealm/scala/HelloWorld.scala
+++ ScalaHW/src/info/drealm/scala/HelloWorld.scala
@@ -5,62 +5,74 @@ import swing.event._
 
 object helloWorld extends SimpleSwingApplication {
 
-    private[this] val integerListView = new SortedListView(Seq(17, 32, 17, 3, 12, 15, 3)) { name = "Integers" }
+    def top(): Frame = new MainFrame {
 
-    private[this] val wordListView = new SortedListView(Seq("Borrower", "Woodshed", "Horse", "Soldier", "Bookend", "Woodshed", "Tomato")) {
-        name = "Words"
-    }
+        case class GoodbyeButtonClicked extends Event;
+        case class DefaultButtonChanged(override val source: Button) extends ComponentEvent()
 
-    private[this] val helloButton = new Button {
-        text = "Hello"
-        mnemonic = Key.E
-        preferredSize = new Dimension((new Button("Goodbye")).preferredSize.width, preferredSize.height)
-    }
+        private[this] val migPanel = new MigPanel("", "[grow]", "[grow]") {
 
-    private[this] val goodbyeButton = new Button {
-        text = "Goodbye"
-        mnemonic = Key.E
-    }
+            private[this] def sortedListViewSelectionChanged[T](sortedListView: SortedListView[T]) = {
+                // Fires on mouseDown and mouseUp..!
+                for (x <- sortedListView.selection.items)
+                    println(x)
+            }
 
-    private[this] val migPanel = new MigPanel("", "[grow]", "[grow]") {
+            private[this] val integerListView = new SortedListView(Seq(17, 32, 17, 3, 12, 15, 3)) { name = "Integers" }
 
-        contents += integerListView
-        contents += wordListView
-        contents += helloButton
+            private[this] val wordListView = new SortedListView(Seq("Borrower", "Woodshed", "Horse", "Soldier", "Bookend", "Woodshed", "Tomato")) {
+                name = "Words"
+            }
 
-        listenTo(integerListView.selection)
-        listenTo(wordListView.selection)
-        listenTo(helloButton)
-        reactions += {
-            case SelectionChanged(`integerListView`) => sortedListViewSelectionChanged(integerListView)
-            case SelectionChanged(`wordListView`) => sortedListViewSelectionChanged(wordListView)
-            case ButtonClicked(`helloButton`) => {
-                helloButton.visible = false
-                contents -= helloButton
-                contents += goodbyeButton
+            // Ugh, compile-order dependent code.  Must define goodbyeButton before
+            // using its preferredSize in helloButton.  It beats defining an extra
+            // pretend-goodbyeButton in helloButton, though.
+            private[this] val goodbyeButton = new Button {
+                text = "Goodbye"
+                mnemonic = Key.E
+            }
+
+            private[this] val helloButton = new Button {
+                text = "Hello"
+                mnemonic = Key.E
+                preferredSize = new Dimension(goodbyeButton.preferredSize.width, preferredSize.height)
+            }
+            
+            def defaultButton: Button = {
+                if (contents.contains(helloButton)) helloButton else goodbyeButton
+            }
+
+            contents += integerListView
+            contents += wordListView
+            contents += helloButton
+
+            listenTo(integerListView.selection)
+            listenTo(wordListView.selection)
+            listenTo(helloButton)
+            listenTo(goodbyeButton)
+            reactions += {
+                case SelectionChanged(`integerListView`) => sortedListViewSelectionChanged(integerListView)
+                case SelectionChanged(`wordListView`) => sortedListViewSelectionChanged(wordListView)
+                case ButtonClicked(`helloButton`) => {
+                    helloButton.visible = false
+                    contents -= helloButton
+                    contents += goodbyeButton
+                    publish(new DefaultButtonChanged(defaultButton))
+                }
+                case ButtonClicked(`goodbyeButton`) => publish(new GoodbyeButtonClicked)
             }
         }
-    }
-
-    private[this] def sortedListViewSelectionChanged[T](sortedListView: SortedListView[T]) = {
-        // Fires on mouseDown and mouseUp..!
-        for (x <- sortedListView.selection.items)
-            println(x)
-    }
-
-    def top(): Frame = new MainFrame {
 
         title = "Hello, world!"
         minimumSize = new Dimension(228, 40)
-        defaultButton = helloButton
         contents = migPanel
+        defaultButton = migPanel.defaultButton
         centerOnScreen
 
         listenTo(migPanel)
-        listenTo(goodbyeButton)
         reactions += {
-            case ComponentAdded(`migPanel`, `goodbyeButton`) => defaultButton = goodbyeButton
-            case ButtonClicked(`goodbyeButton`) => quit()
+            case DefaultButtonChanged(x) => defaultButton = x
+            case GoodbyeButtonClicked() => quit()
         }
     }
 }
