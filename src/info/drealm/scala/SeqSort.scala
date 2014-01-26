ad4102d92e8eb31dc0bfa588ee3917f51e0ba750
Peter L Jones <peter@drealm.info>
2014-01-26 14:39:53 +0000

    Factor out common code between XXSort traits into smaller traits

--- src/info/drealm/scala/SeqSort.scala
+++ src/info/drealm/scala/SeqSort.scala
@@ -2,12 +2,7 @@ package info.drealm.scala
 
 import operator.Functional._
 
-trait SeqSort[+T] {
-
-    // The method has a constraint that items can be ordered and 
-    // therefore have a compare function.
-    def sort[T <% Ordered[T]]()(xs: Traversable[T]): Seq[T] = xs |> sortBy(_.compare(_))
-
+trait AnySort[+T] {
     // This is the unconstrained version where it is up to the
     // caller to explain how to sequence the incoming traversable.
     // The comparator function passed must, however, work like Ordered.compare.
@@ -15,25 +10,31 @@ trait SeqSort[+T] {
         xs match {
             case Nil => Nil
             case _ :: Nil => xs.toSeq
-            case pivot :: rest =>
-                val (lower, higher) = rest partition (comparator(pivot, _) > 0)
-                (lower |> sortBy(comparator)) ++:
-                    Seq(pivot) ++:
-                    (higher |> sortBy(comparator))
+            case _ => xs |> handleTwoOrMore(comparator)
         }
     }
+
+    protected def handleTwoOrMore[T](comparator: (T, T) => Int)(xs: Traversable[T]): Seq[T]
 }
 
-trait MergeSort[+T] {
-
+trait ComparableSort[+T] extends AnySort[T] {
     // The method has a constraint that items can be ordered and 
     // therefore have a compare function.
     def sort[T <% Ordered[T]]()(xs: Traversable[T]): Seq[T] = xs |> sortBy(_.compare(_))
+}
 
-    // This is the unconstrained version where it is up to the
-    // caller to explain how to sequence the incoming traversable.
-    // The comparator function passed must, however, work like Ordered.compare.
-    def sortBy[T](comparator: (T, T) => Int)(xs: Traversable[T]): Seq[T] = {
+trait SeqSort[+T] extends ComparableSort[T] {
+    protected def handleTwoOrMore[T](comparator: (T, T) => Int)(xs: Traversable[T]): Seq[T] = {
+        val pivot = xs.head
+        val (lower, higher) = xs.tail partition (comparator(pivot, _) > 0)
+        (lower |> sortBy(comparator)) ++:
+            Seq(pivot) ++:
+            (higher |> sortBy(comparator))
+    }
+}
+
+trait MergeSort[+T] extends ComparableSort[T] {
+    protected def handleTwoOrMore[T](comparator: (T, T) => Int)(xs: Traversable[T]): Seq[T] = {
 
         def merge(xs: Seq[T], ys: Seq[T]): Seq[T] =
             (xs, ys) match {
@@ -46,12 +47,7 @@ trait MergeSort[+T] {
                         Seq(yh) ++: merge(xs, yt)
             }
 
-        xs match {
-            case Nil => Nil
-            case _ :: Nil => xs.toSeq
-            case _ =>
-                val (left, right) = xs splitAt xs.toSeq.length / 2
-                merge(sortBy(comparator)(left), sortBy(comparator)(right))
-        }
+        val (left, right) = xs splitAt xs.toSeq.length / 2
+        merge(sortBy(comparator)(left), sortBy(comparator)(right))
     }
 }
