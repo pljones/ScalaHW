ddb111fe49fb3315cd43434b080b48a07f5ebe6d
Peter L Jones <peter@drealm.info>
2014-01-26 14:19:58 +0000

    Rename doSort to sort and sortBy; use positional args in sort; use partition rather than filter+filterNot; add MergeSort

--- src/info/drealm/scala/SeqSort.scala
+++ src/info/drealm/scala/SeqSort.scala
@@ -6,21 +6,52 @@ trait SeqSort[+T] {
 
     // The method has a constraint that items can be ordered and 
     // therefore have a compare function.
-    def doSort[T <% Ordered[T]]()(xs: Traversable[T]): Seq[T] = {
-        xs |> doSort((x, y) => x.compare(y))
-    }
+    def sort[T <% Ordered[T]]()(xs: Traversable[T]): Seq[T] = xs |> sortBy(_.compare(_))
 
     // This is the unconstrained version where it is up to the
     // caller to explain how to sequence the incoming traversable.
     // The comparator function passed must, however, work like Ordered.compare.
-    def doSort[T](comparator: (T, T) => Int)(xs: Traversable[T]): Seq[T] = {
+    def sortBy[T](comparator: (T, T) => Int)(xs: Traversable[T]): Seq[T] = {
         xs match {
             case Nil => Nil
-            case head :: Nil => Seq(head)
+            case _ :: Nil => xs.toSeq
             case pivot :: rest =>
-                doSort(comparator)(rest filter (comparator(pivot,_) > 0)) ++:
+                val (lower, higher) = rest partition (comparator(pivot, _) > 0)
+                (lower |> sortBy(comparator)) ++:
                     Seq(pivot) ++:
-                    doSort(comparator)(rest filterNot (comparator(pivot,_) > 0))
+                    (higher |> sortBy(comparator))
+        }
+    }
+}
+
+trait MergeSort[+T] {
+
+    // The method has a constraint that items can be ordered and 
+    // therefore have a compare function.
+    def sort[T <% Ordered[T]]()(xs: Traversable[T]): Seq[T] = xs |> sortBy(_.compare(_))
+
+    // This is the unconstrained version where it is up to the
+    // caller to explain how to sequence the incoming traversable.
+    // The comparator function passed must, however, work like Ordered.compare.
+    def sortBy[T](comparator: (T, T) => Int)(xs: Traversable[T]): Seq[T] = {
+
+        def merge(xs: Seq[T], ys: Seq[T]): Seq[T] =
+            (xs, ys) match {
+                case (Nil, _) => ys
+                case (_, Nil) => xs
+                case (xh :: xt, yh :: yt) =>
+                    if (comparator(xh, yh) < 0)
+                        Seq(xh) ++: merge(xt, ys)
+                    else
+                        Seq(yh) ++: merge(xs, yt)
+            }
+
+        xs match {
+            case Nil => Nil
+            case _ :: Nil => xs.toSeq
+            case _ =>
+                val (left, right) = xs splitAt xs.toSeq.length / 2
+                merge(sortBy(comparator)(left), sortBy(comparator)(right))
         }
     }
 }
--- src/scala/swing/SortedListView.scala
+++ src/scala/swing/SortedListView.scala
@@ -1,16 +1,16 @@
 package scala.swing
 
-import info.drealm.scala.SeqSort
+import info.drealm.scala.{SeqSort,MergeSort}
 import operator.Functional._
 
 class SortedListView[T <% Ordered[T]] extends ListView[T] with SeqSort[T] {
     def this(items: Traversable[T]) = {
         this()
-        listData = items |> doSort()
+        listData = items |> sort()
     }
     
     def this(orderBy: (T, T) => Int, items: Traversable[T]) {
         this()
-        listData = items |> doSort(orderBy)
+        listData = items |> sortBy(orderBy)
     }
 }
